7f9e6e9c4c2758334051989908a90ec1f100548c
lionelz <ariel.zerbib@gmail.com>
2016-06-19 18:32:07 +0300

    fix zeromq

--- networking_bambuk/rpc/zeromq/zeromq_rpc.py
+++ networking_bambuk/rpc/zeromq/zeromq_rpc.py
@@ -13,6 +13,7 @@
 #
 
 import json
+import sys
 import threading
 import zmq
 
@@ -21,55 +22,70 @@ class ZeroMQReceiver(object):
     
     def __init__(self, bambuk_agent):
         self._bambuk_agent = bambuk_agent
+        self._running = True
         thread = threading.Thread(target=self.receive)
         thread.start()
 
     def receive(self):
         context = zmq.Context()
-        socket = context.socket(zmq.REP)
-        socket.bind("tcp://*:5555")
-        while True:
+        self._socket = context.socket(zmq.REP)
+        self._socket.bind("tcp://*:5555")
+        self._socket.RCVTIMEO = 1000
+        while self._running:
             #  Wait for next request from client
-            message_json = socket.recv()
-            print("Received request: %s" % message_json)
-            # call bambuk_agent
-            message = json.loads(message_json)
-            method = message['method']
-            handler = getattr(self, method, None)
-            if handler is not None:
-                del message['method']
-                handler(**message)
+            try:
+                message_json = self._socket.recv()
+                print("Received message: %s" % message_json)
+                # call bambuk_agent
+                message = json.loads(message_json)
+                method = message['method']
+                handler = getattr(self, method, None)
+                if handler is not None:
+                    del message['method']
+                    response = handler(**message)
+                    #  Send reply back to client
+                    response_json = json.dumps(response)
+                    print("Sending response: %s" % response_json)
+                    self._socket.send(response_json)
+            except:
+                print("not received")
 
     def agent_state(self, **kwargs):
-        print("agent_state: %s" % kwargs)
         server_conf = kwargs.get('server_conf')
         return self._bambuk_agent.agent_state(server_conf=server_conf)
 
     def apply(self, **kwargs):
         vm_connectivity = kwargs.get('vm_connectivity')
-        return self._bambuk_agent.apply(vm_connectivity)
+        return self._bambuk_agent.apply(vm_connectivity=vm_connectivity)
 
     def update(self, **kwargs):
         vm_connectivity_update = kwargs.get('vm_connectivity_update')
-        return self._bambuk_agent.update(vm_connectivity_update)
+        return self._bambuk_agent.update(vm_connectivity_update=vm_connectivity_update)
 
     def version(self, **kwargs):
         return self._bambuk_agent.version()
 
+    def close(self):
+        self._running = False
+
 
 class ZeroMQSender(object):
 
-    def __init__(self, host_or_ip):
+    def __init__(self, host_or_ip, port=5555):
         context = zmq.Context()
         self._socket = context.socket(zmq.REQ)
-        self._socket.connect("tcp://%s:5555" % host_or_ip)
+        self._socket.connect("tcp://%s:%d" % (host_or_ip, port))
 
     def call_method(self, method, **kwargs):
-        # TODO: implement it
         message = {'method': method}
         for name, value in kwargs.items():
             message[name] = value
-        return self._socket.send(json.dumps(message))
+        message_json = json.dumps(message)
+        print("Sending message: %s" % message_json)
+        self._socket.send(message_json)
+        response_json = self._socket.recv()
+        print("Received response: %s" % response_json)
+        return json.loads(response_json)
 
     def agent_state(self, server_conf):
         return self.call_method('agent_state', server_conf=server_conf)
--- networking_bambuk/test/rpc/zeromq/test_zeromq_rpc.py
+++ networking_bambuk/test/rpc/zeromq/test_zeromq_rpc.py
@@ -13,6 +13,7 @@
 #
 
 import unittest
+import time
 
 from networking_bambuk.rpc.zeromq import zeromq_rpc
 from networking_bambuk.rpc import bambuk_rpc
@@ -28,17 +29,17 @@ class FakeBambukAgent(bambuk_rpc.BambukRpc):
         self.obj_version = None
 
     def agent_state(self, server_conf):
-        self.server_conf = server_conf.copy()
+        self.server_conf = server_conf
         self.state = {'state': True}
         return self.state
 
     def apply(self, vm_connectivity):
         self.vm_connectivity = vm_connectivity
-        return
+        return True
 
     def update(self, vm_connectivity_update):
         self.vm_connectivity_update = vm_connectivity_update
-        return
+        return True
 
     def version(self):
         self.obj_version = '1.2'
@@ -46,31 +47,42 @@ class FakeBambukAgent(bambuk_rpc.BambukRpc):
 
 class TestZeroMqRpc(unittest.TestCase):
 
-    def setUp(self):
-        self._bambuk_agent = FakeBambukAgent()
-        self._receiver = zeromq_rpc.ZeroMQReceiver(self._bambuk_agent)
-        self._sender = zeromq_rpc.ZeroMQSender('localhost')
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
+        bambuk_agent = FakeBambukAgent()
+        TestZeroMqRpc._receiver = zeromq_rpc.ZeroMQReceiver(bambuk_agent)
+        TestZeroMqRpc._sender = zeromq_rpc.ZeroMQSender('localhost')
+
+    @classmethod
+    def tearDownClass(cls):
+        TestZeroMqRpc._receiver.close()
 
     def test_agent_state(self):
         server_conf = {'server_ip': '10.10.10.10'}
-        state = self._sender.agent_state(server_conf)
-        self.assertEqual(state, self._bambuk_agent.state)
-        self.assertEqual(server_conf, self._bambuk_agent.server_conf)
+        state = TestZeroMqRpc._sender.agent_state(server_conf)
+        self.assertDictEqual(state,
+                             TestZeroMqRpc._receiver._bambuk_agent.state)
+        self.assertDictEqual(server_conf,
+                             TestZeroMqRpc._receiver._bambuk_agent.server_conf)
 
     def test_apply(self):
         vm_connectivity = {'port': 'xxx'}
-        self._sender.apply(vm_connectivity)
-        self.assertEqual(vm_connectivity, self._bambuk_agent.vm_connectivity)
+        TestZeroMqRpc._sender.apply(vm_connectivity)
+        self.assertDictEqual(vm_connectivity,
+                             TestZeroMqRpc._receiver._bambuk_agent.vm_connectivity)
 
     def test_update(self):
         vm_connectivity_update = {'port': 'xxx'}
-        self._sender.update(vm_connectivity_update)
-        self.assertEqual(vm_connectivity_update,
-                         self._bambuk_agent.vm_connectivity_update)
+        TestZeroMqRpc._sender.update(vm_connectivity_update)
+        self.assertDictEqual(
+            vm_connectivity_update,
+            TestZeroMqRpc._receiver._bambuk_agent.vm_connectivity_update)
 
     def test_version(self):
-        version = self._sender.version()
-        self.assertEqual(version, self._bambuk_agent.obj_version)
+        version = TestZeroMqRpc._sender.version()
+        self.assertEqual(version,
+                         TestZeroMqRpc._receiver._bambuk_agent.obj_version)
 
 
 if __name__ == '__main__':
