1998c2bdade3b20c6a2c33f13328088f1effbc5a
Lionel Zerbib <ariel.zerbib@gmail.com>
2016-08-10 13:54:31 +0300

    fix local conf sample

---
+++ devstack/update_port_sample.sh
@@ -0,0 +1,11 @@
+
+source openrc admin demo admin stack
+
+neutron net-list
+
+neutron port-create 0b044286-11d1-456a-a701-d0701c36414c
+
+neutron port-list
+
+neutron port-update c91115f7-2151-4c53-aea9-5f68ccfa2256 --binding:profile type=dict provider_mgnt_ip=192.168.122.117
+
