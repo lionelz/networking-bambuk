595f2ffc9c6e23b19bf5d2a3ab7159d0cc67d67a
Lionel Zerbib <ariel.zerbib@gmail.com>
2017-02-20 08:28:46 +0200

    fix new db api

--- networking_bambuk/agent/df/df_tiny_db.py
+++ networking_bambuk/agent/df/df_tiny_db.py
@@ -1,17 +1,3 @@
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-#
-
 import sys
 
 from dragonflow.db import db_api
@@ -314,3 +300,11 @@ class TinyDbDriver(db_api.DbApi, bambuk_rpc.BambukRpc):
             LOG.error('an error occurs %s' % e)
             return False
         return True
+
+    def process_ha(self):
+        # Not needed in this driver
+        pass
+
+    def set_neutron_server(self, is_neutron_server):
+        # Not needed in this driver
+        pass
